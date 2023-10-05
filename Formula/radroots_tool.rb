400c4b18c72e9e38de14459fcb088bbb7f31122c
triesap <137732411+triesap@users.noreply.github.com>
2023-10-05 23:49:45 +0000

    update working tree

--- Formula/radroots_tool.rb
+++ Formula/radroots_tool.rb
@@ -2,7 +2,7 @@ class RadrootsTool < Formula
     desc "Radroots CLI tool"
     homepage "https://github.com/triesap/rr_radroots_tool"
     url "https://github.com/triesap/rr_radroots_tool/releases/download/v0.0.1/radroots_tool-0.0.1-aarch64-apple-darwin.tar.gz"
-    sha256 "67824e8855ccb67cebb0ca5e3a4bab7063be1548"
+    sha256 "cee5c8a3a8c5ca5ce9833848ea4253319fdd7f8a957def4e750ddf4ea889c85b"
     version "0.0.1"
   
     def install
